3373b855997737ad25f83f8215721543e6e39294
Jefferson fonseca <jefferson.fonseca@casion.com.br>
2024-02-02 15:11:42 -0300

    refactor: url api

--- src/utils/classMerge.ts
+++
@@ -1,6 +0,0 @@
-import { clsx, ClassValue } from 'clsx'
-import { twMerge } from 'tailwind-merge'
-
-export default function classMerge(...classes: ClassValue[]) {
-  return twMerge(clsx(classes))
-}
